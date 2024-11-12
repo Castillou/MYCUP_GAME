cdc9134c3636e68311567a0372e6342f1822df74
Castillou <joajoa70584@gmail.com>
2024-11-12 15:31:25 +0900

    :sparkles:feat(login): 비밀번호 입력란에 마우스 오버 시 input 타입을 text로 바꿔서 비밀번호를 보여주는 눈 아이콘 추가

--- frontend/src/compnents/Login/LoginForm.jsx
+++ frontend/src/compnents/Login/LoginForm.jsx
@@ -7,30 +7,62 @@ import {
 } from 'react-router-dom';
 import styled from 'styled-components';
 import classes from './LoginForm.module.css';
+import { useState } from 'react';
+import eye from '../../assets/eye.svg';
+import eyeS from '../../assets/eye-slash.svg';
 
 const Title = styled.h1`
 	font-size: 6rem;
 	font-weight: 900;
 	text-align: center;
 	padding-left: 1rem;
-	margin-bottom: 2rem;
+	margin-bottom: 4rem;
 `;
 
-const Input = styled.input`
-	font-size: 2rem;
-	padding: 1.7rem 2rem;
-	border: 1px solid #ccc;
-	border-radius: 5rem;
+const InputConatiner = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
 
-	&:focus {
-		outline: 1.5px solid #2e93ff;
+	margin-bottom: 2rem;
+	position: relative;
+
+	input {
+		font-size: 2rem;
+		padding: 1.7rem 2rem;
+		border: 1px solid #ccc;
+		border-radius: 5rem;
+
+		position: relative;
+
+		&:focus {
+			outline: 1.5px solid #2e93ff;
+
+			+ span {
+				display: block;
+			}
+		}
+	}
+
+	span {
+		display: none;
+
+		position: absolute;
+		bottom: 1.45rem;
+		right: 2rem;
+		width: 3rem;
+		height: 3rem;
 	}
 `;
 
 const Button = styled.button`
-	font-size: 2rem;
+	width: 100%;
 	padding: 1.7rem;
+	margin-bottom: 2rem;
+
 	border: none;
+	font-size: 2rem;
+
 	background-color: #007bff;
 	color: white;
 	cursor: pointer;
@@ -60,6 +92,9 @@ export default function LoginForm() {
 	const data = useActionData();
 	const navigation = useNavigation();
 
+	const [showPassword, setShowPassword] = useState('password');
+	const [icon, setIcon] = useState(eye);
+
 	const [searchParams] = useSearchParams();
 	const isLogin = searchParams.get('mode') === 'login';
 	const isSignup = searchParams.get('mode') === 'signup';
@@ -73,6 +108,15 @@ export default function LoginForm() {
 		buttonText = '가입하기';
 	}
 
+	const handleMouseOver = () => {
+		setIcon(eyeS);
+		setShowPassword('text');
+	};
+	const handleMouseOut = () => {
+		setIcon(eye);
+		setShowPassword('password');
+	};
+
 	return (
 		<>
 			<Form method="post" className={classes.form}>
@@ -85,29 +129,34 @@ export default function LoginForm() {
 					</ul>
 				)}
 				{data && data.message && <p>{data.message}</p>}
-				{isSignup && (
-					<Input
-						id="username"
-						type="username"
-						name="username"
-						placeholder="이름"
+				<InputConatiner>
+					{isSignup && (
+						<input
+							id="username"
+							type="username"
+							name="username"
+							placeholder="이름"
+							required
+						/>
+					)}
+					<input
+						id="email"
+						type="email"
+						name="email"
+						placeholder="이메일"
 						required
 					/>
-				)}
-				<Input
-					id="email"
-					type="email"
-					name="email"
-					placeholder="이메일"
-					required
-				/>
-				<Input
-					id="password"
-					type="password"
-					name="password"
-					placeholder="비밀번호"
-					required
-				/>
+					<input
+						id="password"
+						type={showPassword}
+						name="password"
+						placeholder="비밀번호"
+						required
+					/>
+					<span onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+						<img src={icon} alt="눈 아이콘" />
+					</span>
+				</InputConatiner>
 				<Button disabled={isSubmitting}>
 					{isSubmitting ? '로딩중...' : buttonText}
 				</Button>
